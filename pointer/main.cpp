991087a152e5ee2a3886bad38c49044998434f8e
20141105072 <2196690899@qq.com>
2015-12-02 20:49:07 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by 20141105072 on 15/12/2.
+//  Copyright © 2015年 20141105072. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
